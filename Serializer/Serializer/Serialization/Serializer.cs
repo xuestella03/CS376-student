3819958e323d6b8b33b4cbf8d3ca1a2dd877fbe7
Estella Xu <xuestella03@gmail.com>
2024-10-05 16:17:52 -0500

    finished serializing

--- Serializer/Serializer/Serialization/Serializer.cs
+++ Serializer/Serializer/Serialization/Serializer.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 
 namespace Assets.Serialization
 {
@@ -190,28 +191,28 @@ namespace Assets.Serialization
             switch (o)
             {
                 case null:
-                    throw new NotImplementedException("Fill me in");
+                    Write("null");
                     break;
 
                 case int i:
-                    throw new NotImplementedException("Fill me in");
+                    Write(i);
                     break;
 
                 case float f:
-                    throw new NotImplementedException("Fill me in");
+                    Write(f);
                     break;
 
                 // Not: don't worry about handling strings that contain quote marks
                 case string s:
-                    throw new NotImplementedException("Fill me in");
+                    Write("\"" + s + "\"");
                     break;
 
                 case bool b:
-                    throw new NotImplementedException("Fill me in");
+                    Write(b);
                     break;
 
                 case IList list:
-                    throw new NotImplementedException("Fill me in");
+                    WriteList(list);
                     break;
 
                 default:
@@ -231,7 +232,29 @@ namespace Assets.Serialization
         /// <param name="o">Object to serialize</param>
         private void WriteComplexObject(object o)
         {
-            throw new NotImplementedException("Fill me in");
+            var id = GetId(o);
+            Write("#" + id.Item1);
+            if (id.Item2 == false) { // Case where it's already been output
+                return;
+            }
+            var fields = Utilities.SerializedFields(o);
+            //var fieldList = "";
+
+            WriteBracketedExpression(
+                "{ ",
+                () =>
+                {
+                    WriteField("type", o.GetType().Name, true);
+                    foreach (var field in fields)
+                    {
+                        WriteField(field.Key, field.Value, false);
+                    }
+                },
+                " }");
+            //foreach (var field in fields)
+            //{
+            //    fieldList += WriteField(field.Key, field.Value, false);
+            //}
         }
     }
 }
